0d7b5cde7f4f3b939f15ff387fbe8e48f8c17ff2
Aaron Martinez <aaronmartinez6@hotmail.com>
2018-01-22 20:13:56 -0700

    Finish project

--- src/App.js
+++ src/App.js
@@ -1,49 +1,55 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+
 import "./App.css";
+
+import { decrement, increment, undo, redo } from "./ducks/counter";
 
 class App extends Component {
   render() {
+    const { currentValue, decrement, increment, undo, redo } = this.props;
+
     return (
       <div className="app">
         <section className="counter">
-          <h1 className="counter__current-value">{ 0 }</h1>
+          <h1 className="counter__current-value">{ currentValue }</h1>
           <div className="counter__button-wrapper">
             <button
-              className="counter__button increment-one"
-              onClick={ () => null }
+              className="counter__button"
+              onClick={ () => increment( 1 ) }
             >
               +1
             </button>
             <button
-              className="counter__button increment-five"
-              onClick={ () => null }
+              className="counter__button"
+              onClick={ () => increment( 5 ) }
             >
               +5
             </button>
             <button
-              className="counter__button decrement-one"
-              onClick={ () => null }
+              className="counter__button"
+              onClick={ () => decrement( 1 ) }
             >
               -1
             </button>
             <button
-              className="counter__button decrement-five"
-              onClick={ () => null }
+              className="counter__button"
+              onClick={ () => decrement( 5 ) }
             >
               -5
             </button>
             <br />
             <button
-              className="counter__button undo"
-              disabled={ true }
-              onClick={ () => null }
+              className="counter__button"
+              disabled={ this.props.previousValues.length === 0 }
+              onClick={ undo }
             >
               Undo
             </button>
             <button
-              className="counter__button redo"
-              disabled={ true }
-              onClick={ () => null }
+              className="counter__button"
+              disabled={ this.props.futureValues.length === 0 }
+              onClick={ redo }
             >
               Redo
             </button>
@@ -59,4 +65,9 @@ class App extends Component {
   }
 }
 
-export default App;
+function mapStateToProps( state ) {
+  console.log(state)
+  return state;
+}
+
+export default connect( mapStateToProps, { decrement, increment, undo, redo } )( App );
